6d41fb04d13514334466a474156f71c117623527
cedricvasset <vasset.cedric@orange.fr>
2018-12-15 15:27:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//$comp = 200;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part3 exe8</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <?php for($comp = 200 ; $comp >= 0 ; $comp -= 12 ){ ?>
+    <p>Enfin !!!!!!!!!!!</p>
+  <?php } ?>
+
+  <?php // while($comp >= 0) { ?>
+    <!-- <p><?php //echo ' Enfin !!!!.'; $comp -= 12 ?></p> -->
+    <?php //} ?>
+  </body>
+  </html>
